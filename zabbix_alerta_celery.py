eb6ca57369f6b1d99110476dbd16eccf40e5284e
Veeresh R Khanorkar <veeresh.r.khanorkar@ericsson.com>
2019-02-28 14:35:50 +0000

    Implemented multiple keys detection in alerte events as well as correctly ordering from ok->high->disaster->ok

--- zabbix_alerta_celery.py
+++ zabbix_alerta_celery.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 """
-    zabbix-alerta-celery: Forward Zabbix events to Alerta via celery
+  zabbix-alerta-celery: Forward Zabbix events to Alerta via celery
 """
 
 import os
 import sys
 import argparse
 import logging as LOG
+from pyzabbix import ZabbixAPI
 
 try:
-    import configparser
+  import configparser
 except ImportError:
-    import ConfigParser as configparser
+  import ConfigParser as configparser
 
 from alertaclient.api import Client
 from zabbix_celery import send2celery
@@ -23,26 +24,26 @@ LOG_FORMAT = "%(asctime)s.%(msecs).03d %(name)s[%(process)d] %(threadName)s %(le
 LOG_DATE_FMT = "%Y-%m-%d %H:%M:%S"
 
 OPTIONS = {
-    'config_file': '~/.alerta.conf',
-    'profile':     None,
-    'endpoint':    'http://localhost:8080',
-    'key':         '',
-    'sslverify':   True,
-    'debug':       False
+  'config_file': '~/.alerta.conf',
+  'profile':   None,
+  'endpoint':  'http://localhost:8080',
+  'key':     '',
+  'sslverify':  True,
+  'debug':    False
 }
 
 ZBX_SEVERITY_MAP = {
-    'Disaster':       'critical',
-    'High':           'major',
-    'Average':        'minor',
-    'Warning':        'warning',
-    'Information':    'informational',
-    'Not classified': 'indeterminate'
+  'Disaster':    'critical',
+  'High':      'major',
+  'Average':    'minor',
+  'Warning':    'warning',
+  'Information':  'informational',
+  'Not classified': 'indeterminate'
 }
 
 epilog = '''INSTALL
 
-   $ ln -s `which zabbix-alerta` <AlertScriptsPath>
+  $ ln -s `which zabbix-alerta` <AlertScriptsPath>
 
 ALERT FORMAT
 
@@ -101,129 +102,172 @@ dateTime={EVENT.RECOVERY.DATE}T{EVENT.RECOVERY.TIME}Z
 
 def parse_zabbix(subject, message):
 
-    alert = {}
-    attributes = {}
-    zabbix_severity = False
-    for line in message.split('\n'):
-        if '=' not in line:
-            continue
-        try:
-            macro, value = line.rstrip().split('=', 1)
-        except ValueError as e:
-            LOG.warning('%s: %s', e, line)
-            continue
+  alert = {}
+  attributes = {}
+  zabbix_severity = False
+  for line in message.split('\n'):
+    if '=' not in line:
+      continue
+    try:
+      macro, value = line.rstrip().split('=', 1)
+    except ValueError as e:
+      LOG.warning('%s: %s', e, line)
+      continue
 
-        if macro == 'service':
-            value = value.split(', ')
-        elif macro == 'severity':
-            if value.endswith('!!'):
-                zabbix_severity = True
-                value = value.replace('!!','')
-            else:
-                value = ZBX_SEVERITY_MAP.get(value, 'indeterminate')
-        elif macro == 'tags':
-            value = value.split(', ')
-        elif macro.startswith('attributes.'):
-            attributes[macro.replace('attributes.', '')] = value
+    if macro == 'service':
+      value = value.split(', ')
+    elif macro == 'severity':
+      if value.endswith('!!'):
+        zabbix_severity = True
+        value = value.replace('!!','')
+      else:
+        value = ZBX_SEVERITY_MAP.get(value, 'indeterminate')
+    elif macro == 'tags':
+      value = value.split(', ')
+    elif macro.startswith('attributes.'):
+      attributes[macro.replace('attributes.', '')] = value
 
-        alert[macro] = value
-        LOG.debug('%s -> %s', macro, value)
+    alert[macro] = value
+    LOG.debug('%s -> %s', macro, value)
 
-    # if {$ENVIRONMENT} user macro isn't defined anywhere set default
-    if alert.get('environment', '') == '{$ENVIRONMENT}':
-        alert['environment'] = 'Production'
+  # if {$ENVIRONMENT} user macro isn't defined anywhere set default
+  if alert.get('environment', '') == '{$ENVIRONMENT}':
+    alert['environment'] = 'Production'
 
-    zabbix_status = alert.pop('status', None)
+  zabbix_status = alert.pop('status', None)
 
-    # if zabbix_status == 'OK':
-        # if zabbix_severity:
-            # alert['severity'] = 'ok'
-        # else:
-            # alert['severity'] = 'normal'
+  if zabbix_status == 'OK':
+    if zabbix_severity:
+      alert['severity'] = 'OK'
+    else:
+      alert['severity'] = 'normal'
 
-    if alert.pop('ack', '') == 'Yes' and zabbix_status != 'OK':
-        alert['status'] = 'ack'
+  if alert.pop('ack', '') == 'Yes' and zabbix_status != 'OK':
+    alert['status'] = 'ack'
 
-    alert['attributes'] = attributes
-    alert['origin'] = "zabbix/%s" % os.uname()[1]
-    alert['rawData'] = "%s\n\n%s" % (subject, message)
+  alert['attributes'] = attributes
+  alert['origin'] = "zabbix/%s" % os.uname()[1]
+  alert['rawData'] = "%s\n\n%s" % (subject, message)
 
-    return alert
+  return alert
 
 
 def main():
+  config_file = os.environ.get('ALERTA_CONF_FILE') or OPTIONS['config_file']
 
-    config_file = os.environ.get('ALERTA_CONF_FILE') or OPTIONS['config_file']
+  config = configparser.RawConfigParser(defaults=OPTIONS)
+  try:
+    config.read(os.path.expanduser(config_file))
+  except Exception:
+    sys.exit("Problem reading configuration file %s - is this an ini file?" % config_file)
 
-    config = configparser.RawConfigParser(defaults=OPTIONS)
+  parser = argparse.ArgumentParser(
+    prog='zabbix-alerta-celery',
+    usage='zabbix-alerta-celery SENDTO SUMMARY BODY',
+    description='Zabbix-to-Alerta via celery integration script',
+    epilog=epilog,
+    formatter_class=argparse.RawTextHelpFormatter
+  )
+  parser.add_argument(
+    'sendto',
+    help='config profile or alerta API endpoint and key'
+  )
+  parser.add_argument(
+    'summary',
+    help='alert summary'
+  )
+  parser.add_argument(
+    'zbxuser',
+    help='zabbix user'
+  )
+  parser.add_argument(
+    'zbxpassword',
+    help='zabbix password'
+  )
+  parser.add_argument(
+    'zbxurl',
+    help='zabbix url'
+  )  
+  parser.add_argument(
+    'body',
+    help='alert body (see format below)'
+  )
+  args, left = parser.parse_known_args()
+
+  # sendto=apiUrl[;key]
+  if args.sendto.startswith('http://') or args.sendto.startswith('https://'):
+    want_profile = None
     try:
-        config.read(os.path.expanduser(config_file))
-    except Exception:
-        sys.exit("Problem reading configuration file %s - is this an ini file?" % config_file)
+      OPTIONS['endpoint'], OPTIONS['key'] = args.sendto.split(';', 1)
+    except ValueError:
+      OPTIONS['endpoint'] = args.sendto
+  # sendto=profile
+  else:
+    want_profile = args.sendto or os.environ.get('ALERTA_DEFAULT_PROFILE') or config.defaults().get('profile')
 
-    parser = argparse.ArgumentParser(
-        prog='zabbix-alerta-celery',
-        usage='zabbix-alerta-celery SENDTO SUMMARY BODY',
-        description='Zabbix-to-Alerta via celery integration script',
-        epilog=epilog,
-        formatter_class=argparse.RawTextHelpFormatter
-    )
-    parser.add_argument(
-        'sendto',
-        help='config profile or alerta API endpoint and key'
-    )
-    parser.add_argument(
-        'summary',
-        help='alert summary'
-    )
-    parser.add_argument(
-        'body',
-        help='alert body (see format below)'
-    )
-    args, left = parser.parse_known_args()
-
-    # sendto=apiUrl[;key]
-    if args.sendto.startswith('http://') or args.sendto.startswith('https://'):
-        want_profile = None
+    if want_profile and config.has_section('profile %s' % want_profile):
+      for opt in OPTIONS:
         try:
-            OPTIONS['endpoint'], OPTIONS['key'] = args.sendto.split(';', 1)
-        except ValueError:
-            OPTIONS['endpoint'] = args.sendto
-    # sendto=profile
+          OPTIONS[opt] = config.getboolean('profile %s' % want_profile, opt)
+        except (ValueError, AttributeError):
+          OPTIONS[opt] = config.get('profile %s' % want_profile, opt)
     else:
-        want_profile = args.sendto or os.environ.get('ALERTA_DEFAULT_PROFILE') or config.defaults().get('profile')
+      for opt in OPTIONS:
+        try:
+          OPTIONS[opt] = config.getboolean('DEFAULT', opt)
+        except (ValueError, AttributeError):
+          OPTIONS[opt] = config.get('DEFAULT', opt)
 
-        if want_profile and config.has_section('profile %s' % want_profile):
-            for opt in OPTIONS:
-                try:
-                    OPTIONS[opt] = config.getboolean('profile %s' % want_profile, opt)
-                except (ValueError, AttributeError):
-                    OPTIONS[opt] = config.get('profile %s' % want_profile, opt)
-        else:
-            for opt in OPTIONS:
-                try:
-                    OPTIONS[opt] = config.getboolean('DEFAULT', opt)
-                except (ValueError, AttributeError):
-                    OPTIONS[opt] = config.get('DEFAULT', opt)
+  parser.set_defaults(**OPTIONS)
+  args = parser.parse_args()
 
-    parser.set_defaults(**OPTIONS)
-    args = parser.parse_args()
+  if args.debug or not os.path.isdir('/var/log/zabbix'):
+    LOG.basicConfig(stream=sys.stderr, format=LOG_FORMAT, datefmt=LOG_DATE_FMT, level=LOG.DEBUG)
+  else:
+    LOG.basicConfig(filename=LOG_FILE, format=LOG_FORMAT, datefmt=LOG_DATE_FMT, level=LOG.INFO)
 
-    if args.debug or not os.path.isdir('/var/log/zabbix'):
-        LOG.basicConfig(stream=sys.stderr, format=LOG_FORMAT, datefmt=LOG_DATE_FMT, level=LOG.DEBUG)
-    else:
-        LOG.basicConfig(filename=LOG_FILE, format=LOG_FORMAT, datefmt=LOG_DATE_FMT, level=LOG.INFO)
-
-    LOG.info("[alerta] endpoint=%s key=%s  sendto=%s, summary=%s, body=%s", args.endpoint, args.key, args.sendto, args.summary, args.body)
-    try:
-        alert = parse_zabbix(args.summary, args.body)
-        send2celery.delay(args.endpoint, args.key, args.sslverify, alert)
-    except (SystemExit, KeyboardInterrupt):
-        LOG.warning("Exiting zabbix-alerta.")
-        sys.exit(0)
-    except Exception as e:
-        LOG.error(e, exc_info=1)
-        sys.exit(1)
+  LOG.info("[alerta] endpoint=%s key=%s sendto=%s, summary=%s, body=%s", args.endpoint, args.key, args.sendto, args.summary, args.body)
+  try:
+    # log = LOG.getLogger('pyzabbix')
+    # log.setLevel(LOG.DEBUG)
+    alert = parse_zabbix(args.summary, args.body)
+    zapi = ZabbixAPI(args.zbxurl)
+    zapi.session.verify = False
+    zapi.timeout = 300
+    zapi.login(args.zbxuser, args.zbxpassword)
+    trigger = zapi.trigger.get(triggerids=alert['triggerId'], selectFunctions="extend", expandExpression=True)
+    hostId = None
+    items = {}
+    for item in zapi.item.get(itemids=[item['itemid'] for item in trigger[0]['functions']]):
+      hostId = item['hostid']
+      items[item['key_']]=item['itemid']
+    alert['event'] = "_".join(sorted(items.keys()))
+    if alert['severity'] == 'OK':
+      num2Severity = {
+        '0':'Not classified',
+        '1':'Information',
+        '2':'Warning',
+        '3':'Average',
+        '4':'High',
+        '5':'Disaster'
+      }
+      trigger = zapi.trigger.get(monitored=True, hostids=hostId, itemids=[items[key] for key in items.keys()],expandExpression=True, filter={'value':1}, sortfield="priority", sortorder="DESC", limit=1)
+      if len(trigger) > 0:
+        foundAll = True
+        for item in items:
+          if item not in trigger[0]['expression']:
+            foundAll = False
+            break
+        if foundAll:
+          alert['severity'] = num2Severity[trigger[0]['priority']]
+    # LOG.info("Sending to celery")
+    send2celery.apply_async(args = [args.endpoint, args.key, args.sslverify, alert], queue='zabbix_celery')
+  except (SystemExit, KeyboardInterrupt):
+    LOG.warning("Exiting zabbix-alerta.")
+    sys.exit(0)
+  except Exception as e:
+    LOG.error(e, exc_info=1)
+    sys.exit(1)
 
 if __name__ == '__main__':
-    main()
+  main()
